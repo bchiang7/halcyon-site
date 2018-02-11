dcad41f855f232065e4210d0d78e00ea53b5277c
Brittany Chiang <brittany.chiang@gmail.com>
2018-02-11 18:16:41 -0500

    fix prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,6 @@ module.exports = {
   siteMetadata: {
     title: 'Halcyon'
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`]
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`],
+  pathPrefix: `/halcyon-site`
 };
